b73c8cb6201a215a9797c676e33cb4dac2b71b15
Rtrojan Nuke <arimiyahmubarak@gmail.com>
2020-01-28 11:17:42 +0100

    NA-B10: CREATED TEST FOR USER SIGN IN

---
+++ src/test/java/applicant/signin.java
@@ -0,0 +1,16 @@
+package applicant;
+
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class signin extends TestBase{
+	
+	@Test
+	public void signin() {
+	
+		signin();
+		
+	}
+
+}
