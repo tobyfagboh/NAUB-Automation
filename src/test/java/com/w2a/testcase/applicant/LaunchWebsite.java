1b021cab0611f83bfafea6af74001896cc0c10a0
Omoniyi Osadare <osadareomoniyi@gmail.com>
2020-01-28 13:55:03 +0100

    NA-A07 : created test script for Privacy Policy

--- src/test/java/com/w2a/testcase/applicant/LaunchWebsite.java
+++ src/test/java/com/w2a/testcase/applicant/LaunchWebsite.java
@@ -1,6 +1,7 @@
 package com.w2a.testcase.applicant;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import com.w2a.base.TestBase;
@@ -11,14 +12,23 @@ public class LaunchWebsite extends TestBase {
 	@Test
 	public void launchWebsite () throws InterruptedException {
 		
-		driver.findElement(By.xpath(OR.getProperty("contactmenu_XPATH"))).click();
+		
+		
+		/* driver.findElement(By.xpath(OR.getProperty("contactmenu_XPATH"))).click();
 		driver.findElement(By.xpath(OR.getProperty("howtoPay_XPATH"))).click();
 		driver.findElement(By.xpath(OR.getProperty("utme/deRemedial_XPATH"))).click();
-		driver.findElement(By.xpath(OR.getProperty("studentGuide_XPATH"))).click();
+		driver.findElement(By.xpath(OR.getProperty("studentGuide_XPATH"))).click(); 
+		*/
+	
 		
-		Thread.sleep(3000);
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
+		
+		Thread.sleep(5000);
 
+		driver.findElement(By.xpath(OR.getProperty("privacyPolicy_XPATH"))).click();
 		
+		Thread.sleep(5000);
+
 		
 	}
 	
